6975fdc33a6d73faad75dd6c6301adbb3218b997
Eyassub <eyassuberhanu@gmail.com>
2022-04-08 09:53:22 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - gives numers from 0 to 9
+* Return: 0 is a success
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
+
