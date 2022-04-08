3152a876c9b737ad569fa2f31d2ddac50b9e9252
Eyassub <eyassuberhanu@gmail.com>
2022-04-08 09:39:16 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - gives numers from 0 to 9
+* Return: 0 is a success
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; ch++)
+printf("%d \n", n);
+return (0);
+}
+
