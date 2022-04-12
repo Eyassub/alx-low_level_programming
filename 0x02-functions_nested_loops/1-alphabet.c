eb9afce356a1311552e80e8afd3ec4f1bf875689
Eyassub <eyassuberhanu@gmail.com>
2022-04-12 07:15:00 -0700

    2. Lowercase Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - gives alphabets in lowercase from a to z
+* Return: 0 is a success
+*/
+void print_alphabet(void);
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n')
+return (0);
+}
