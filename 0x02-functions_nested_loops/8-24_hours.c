9a5f6e2256fac9c76458b2a66cc6475845bfa29c
Eyassub <eyassuberhanu@gmail.com>
2022-04-12 09:51:45 -0700

    8. Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@
 void jack_bauer(void)
 {  
 int H, M;
-for (H = 0; H < 24; h++)
+for (H = 0; H < 24; H++)
 {
-for (M = 0; M < 60; m++)
+for (M = 0; M < 60; M++)
 {
 _putchar((H / 10) + '0');
 _putchar((H % 10) + '0');
