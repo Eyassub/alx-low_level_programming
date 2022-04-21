354e2feebea18a2b2c9836dd4b06b7409aa719ad
Eyassub <eyassuberhanu@gmail.com>
2022-04-21 13:39:58 -0700

    11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* infinite_add - function with 4 arguments
+* @n1: char type pointer argument
+* @n2: char type pointer argument
+* @r: char type pointer argument
+* @size_r: int type argument
+* Description: adds two numbers from string
+* Return: sum of two integers
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int i, j;
+while (n1[i] != '\0')
+i++;
+while (n2[j] != '\0')
+j++;
+*r = *(r + size_r);
+while (n1[i] > 0 || n1[j] > 0)
+{
+if (n1[i] + n2[j] > 0)
+*r = n1[i - 1] + n2[j - 1] + 1;
+else
+*r = n1[i] + n2[j];
+i--;
+j--;
+}
+return (r);
+}
