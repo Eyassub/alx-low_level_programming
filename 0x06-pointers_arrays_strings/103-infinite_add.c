7287dbb3e5dd9982164af8b5958f1ffbc68bbdfd
Eyassub <eyassuberhanu@gmail.com>
2022-04-21 13:55:09 -0700

    11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,20 +10,36 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j;
-while (n1[i] != '\0')
-i++;
-while (n2[j] != '\0')
-j++;
-*r = *(r + size_r);
-while (n1[i] > 0 || n1[j] > 0)
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
-if (n1[i] + n2[j] > 0)
-*r = n1[i - 1] + n2[j - 1] + 1;
-else
-*r = n1[i] + n2[j];
-i--;
-j--;
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
 }
 return (r);
 }
