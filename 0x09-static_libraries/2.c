9c548c2b958288c0e367410709285730ca876bc5
Eyassub <eyassuberhanu@gmail.com>
2022-04-29 08:09:05 -0700

    0

---
+++ 0x09-static_libraries/2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int _isdigit(int c)
+{
+return 0;
+}
+int _strlen(char *s)
+{
+return 0;
+}
+void _puts(char *s)
+{
+}
+char *_strcpy(char *dest, char *src)
+{
+return "hello";
+}
+int _atoi(char *s)
+{
+return 0;
+}
---
+++ 0x09-static_libraries/4.c
@@ -0,0 +1,21 @@
+#include "main.h"
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return "Hello";
+}
+char *_strchr(char *s, char c)
+{
+return "Hello";
+}
+unsigned int _strspn(char *s, char *accept)
+{
+return 0;
+}
+char *_strpbrk(char *s, char *accept)
+{
+return "Hello";
+}
+char *_strstr(char *haystack, char *needle)
+{
+return "Hello";
+}
