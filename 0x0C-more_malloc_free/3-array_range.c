23f14387048f2ac12d9c8bf4c3c5d4d9ef8692de
Eyassub <eyassuberhanu@gmail.com>
2022-05-06 07:08:11 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,17 +8,14 @@
 */
 int *array_range(int min, int max)
 {
-int i, l;
-int *p;
+int *p, i;
 if (min > max)
 return (NULL);
-l = max - min + 1;
-p = malloc(sizeof(int) * l);
+i = max - min + 1;
+p = malloc(sizeof(*p) * l);
 if (p == NULL)
 return (NULL);
-for (i = 0; i < l; i++; min++)
-{
-p[i] = min;
-}
+while (n--)
+p[i] = max;
 return (p);
 }
